6543e351c19511531090337e2dea542de6ac00ba
kimsaebom <ksb3145@gmail.com>
2016-12-09 00:17:38 +0900

    Revert "정은교 프로잭트 - 필라테스 feat.김준성"
    This reverts commit 747a02ddc78cc5d10edd3a8f8cf4f9e94361efeb.

--- ksb/jeg_project/adm_w/inc/rnb.php
+++
@@ -1,8 +0,0 @@
-<div class="rnb_wrap"><!-- rnb_wrap start -->
-<ul class="rnb">
-	<li><a href="#"><img src="<?=$adm_root?>/image/rnb_btn_01.gif" alt="팝업디자인 신청" /></a></li>
-	<li><a href="#"><img src="<?=$adm_root?>/image/rnb_btn_02.gif" alt="FTP 이용신청" /></a></li>
-	<li><a href="#"><img src="<?=$adm_root?>/image/rnb_btn_03.gif" alt="디자인소스 다운로드" /></a></li>
-	<li><a href="#"><img src="<?=$adm_root?>/image/rnb_btn_04.gif" alt="사용방법 내려받기" /></a></li>
-</ul>
-</div><!-- rnb_wrap end -->
